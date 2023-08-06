e3b47d256708698cda0b09f80945a368929a2ce1
Giftson Jebaraj <giftson1999@gmail.com>
2023-08-06 21:04:59 +0530

    content projection complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Course } from "./model/course";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  coreCourse = COURSES[0];
-  rxjsCourse = COURSES[1];
-  ngrxCourse = COURSES[2];
+  courses = COURSES;
+  onCourseSelected(event) {
+    console.log("App level", event);
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input } from "@angular/core";
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+} from "@angular/core";
+import { CourseImageComponent } from "../course-image/course-image.component";
 import { Course } from "../model/course";
 
 @Component({
@@ -6,9 +17,24 @@ import { Course } from "../model/course";
   templateUrl: "./course-card.component.html",
   styleUrls: ["./course-card.component.css"],
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit, AfterContentInit {
   @Input({
     required: true,
   })
   course: Course;
+
+  @Output("courseSelected")
+  courseEmitter = new EventEmitter<Course>();
+
+  @ContentChildren(CourseImageComponent)
+  images: QueryList<CourseImageComponent>;
+
+  onCourseView() {
+    console.log("Card component la button clicked");
+    this.courseEmitter.emit(this.course);
+  }
+  ngAfterViewInit(): void {}
+  ngAfterContentInit(): void {
+    console.log(this.images);
+  }
 }
